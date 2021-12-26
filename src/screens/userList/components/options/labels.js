13f97bf2bd9af3960fdb9743c7f068637da3a6a6
Diego Alejandro Plaza Garrido <dplazag.agilesoft@bancochile.cl>
2021-12-25 21:49:40 -0300

    🚀 feat: add options component

---
+++ src/screens/userList/components/options/labels.js
@@ -0,0 +1,6 @@
+const labels = {
+    btnNuevoContacto: "Nuevo Contacto",
+    descBuscar: "Buscar contacto..."
+};
+
+export default labels;
---
+++ src/screens/userList/components/options/options.component.js
@@ -0,0 +1,32 @@
+import { Box, Button, InputAdornment, OutlinedInput } from "@mui/material";
+import React from "react";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { FaSearch } from "react-icons/fa";
+import labels from "./labels";
+
+export const options = ({handleOpen}) => {
+  return (
+    <Box className="header">
+      <OutlinedInput
+        className="header__searchBar"
+        placeholder={labels.descBuscar}
+        color="primary"
+        startAdornment={
+          <InputAdornment position="start">
+            <FaSearch className="iconSearch" size='1.3em'/>
+          </InputAdornment>
+        }
+      />
+      <Button
+        className="header__button"
+        variant="contained"
+        startIcon={<AiFillPlusCircle />}
+        onClick={handleOpen}
+      >
+        {labels.btnNuevoContacto}
+      </Button>
+    </Box>
+  );
+};
+
+export default options;

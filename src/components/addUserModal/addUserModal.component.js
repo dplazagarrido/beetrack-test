253feece70f4519c53b370374a384f9b95710205
Diego Alejandro Plaza Garrido <dplazag.agilesoft@bancochile.cl>
2021-12-25 21:50:34 -0300

    🚀 feat: add addUserModal component

---
+++ src/components/addUserModal/addUserModal.component.js
@@ -0,0 +1,77 @@
+import {
+  Button,
+  Divider,
+  FormControl,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import labels from "../labels";
+
+export const AddUserModal = ({ openModal, handleOpen }) => {
+  return (
+    <Box className="contentModal">
+      <Modal
+        open={openModal}
+        onClose={handleOpen}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className="modal">
+          <Box className="modal__header">
+            <Typography
+              className="modal__header__text"
+              variant="h6"
+              component="h2"
+            >
+              {labels.txtNuevoContacto}
+            </Typography>
+          </Box>
+          <Divider></Divider>
+          <Box className="modal__form">
+            <form>
+              <FormControl className="modal__form__formControl" required>
+                <TextField
+                  label={labels.lblUrl}
+                  //value={name}
+                  // onChange={handleChange}
+                  required
+                />
+              </FormControl>
+              <FormControl className="modal__form__formControl" required>
+                <TextField
+                  label={labels.lblNombre}
+                  //value={name}
+                  // onChange={handleChange}
+                  required
+                />
+              </FormControl>
+              <FormControl className="modal__form__formControl" required>
+                <TextField
+                  label={labels.lblDescp}
+                  //value={name}
+                  // onChange={handleChange}
+                  required
+                  multiline
+                  rows={4}
+                />
+              </FormControl>
+              <Box className="modal__buttonBox">
+                <Button
+                  type="submit"
+                  variant="contained"
+                  className="modal__buttonBox__button"
+                  // onClick={this.props.onSubmit}
+                >
+                  {labels.lblBtn}
+                </Button>
+              </Box>
+            </form>
+          </Box>
+        </Box>
+      </Modal>
+    </Box>
+  );
+};
---
+++ src/components/labels.js
@@ -0,0 +1,9 @@
+const labels = {
+    txtNuevoContacto: "Agregar nuevo Contacto",
+    lblUrl: "URL Imagen de perfil",
+    lblNombre: "Nombre",
+    lblDescp: "Descripción",
+    lblBtn: "Guardar",
+};
+
+export default labels;
